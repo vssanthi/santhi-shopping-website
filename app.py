2f646fa79c58a39c2a0474b5a1ae4e90bcb305f1
vssanthi <146327773+vssanthi@users.noreply.github.com>
2023-09-28 11:28:28 +0000

    added the html file

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "Hello I am santhi trying to build a website"
+  return render_template("home.html")
 
 
 if __name__ == '__main__':
