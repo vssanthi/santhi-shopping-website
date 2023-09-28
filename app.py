cdb2ff6e47e1addbe8aa0c494a24d224237c9a75
vssanthi <146327773+vssanthi@users.noreply.github.com>
2023-09-28 10:43:11 +0000

    Added the Flask project

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+  return "Hello I am santhi trying to build a website"
+
+
+if __name__ == '__main__':
+  app.run('0.0.0.0', debug=True)
